6790996967d3ca7efe01a080dcf01d2f31990c3e
iamjinpark <wldls7754@gmail.com>
2025-03-27 21:33:10 +0900

    feat : 메인에 username 연동

--- src/components/profile/Greeting.jsx
+++ src/components/profile/Greeting.jsx
@@ -1,3 +1,5 @@
+import pb from "@/api/pocketbase.js";
+
 function Greeting() {
   const hours = new Date().getHours();
 
@@ -8,13 +10,15 @@ function Greeting() {
     greetingMessage = "GOOD EVENING";
   }
 
+  const userName = pb.authStore.model.name; // 로그인된 유저의 id
+
   return (
     <div className="w-full h-[80px] pl-4 pt-2">
       <h1 className="text-tomato text-3xl font-extrabold transition-all duration-500">
         <span>{greetingMessage},</span>
         <br />
         {/* TODO : 로그인 구현 후 이름 추가 */}
-        <span>NAME!</span>
+        <span>{userName}</span>
       </h1>
     </div>
   );
